183a1b782486d751d69d4d490fd9a4a266047dfe
Víctor Albán <victor.af1104@gmail.com>
2021-12-13 19:01:00 +0100

    Añade un componente relacionado con los inputs y outputs

--- ej-angular/ejemplos-angular/src/app/app.module.ts
+++ ej-angular/ejemplos-angular/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { Cmp01DataBindingComponent } from './cmp01-data-binding/cmp01-data-binding.component';
 import { Cmp02VariablesDePlantillaComponent } from './cmp02-variables-de-plantilla/cmp02-variables-de-plantilla.component';
 import { ReproductorComponent } from './cmp02-variables-de-plantilla/reproductor/reproductor.component';
+import { Cmp03InputYOutputComponent } from 'src/app/cmp03-imput-y-output/cmp03-input-y-output.component';
+import { SugusComponent } from './cmp03-imput-y-output/sugus/sugus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Cmp01DataBindingComponent,
     Cmp02VariablesDePlantillaComponent,
-    ReproductorComponent
+    ReproductorComponent,
+    Cmp03InputYOutputComponent,
+    SugusComponent
   ],
   imports: [
     BrowserModule,
