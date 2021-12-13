7f4c50a707fe3cdaf71cc321eef407f5e8069182
Víctor Albán <victor.af1104@gmail.com>
2021-12-13 13:55:15 +0100

    Añade un componente relacionado con el data binding

--- ej-angular/ejemplos-angular/src/app/app.module.ts
+++ ej-angular/ejemplos-angular/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { Cmp01DataBindingComponent } from './cmp01-data-binding/cmp01-data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Cmp01DataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ ej-angular/ejemplos-angular/src/app/cmp01-data-binding/cmp01-data-binding.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cmp01-data-binding',
+  templateUrl: './cmp01-data-binding.component.html',
+  styleUrls: ['./cmp01-data-binding.component.css']
+})
+export class Cmp01DataBindingComponent implements OnInit {
+
+  nombre: string = 'Charles Falco';
+  nombre2: string = '';
+  deshabilitarInput: boolean = true;
+
+  logo = {
+    nombre: 'Angular',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png'
+  };
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  getNombre(): string {
+    return this.nombre;
+  }
+
+  cambiarNombre(nuevoNombre: string, event: Event): void {
+    console.log(event);
+
+    this.nombre = nuevoNombre;
+  }
+
+  actualizarNombre(event: any): void {
+    this.nombre = event.target.value;
+  }
+}
