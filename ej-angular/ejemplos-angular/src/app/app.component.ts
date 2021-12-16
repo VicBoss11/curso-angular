9b8c5753ef16d5b3ea490210d2615b7af38697f2
Víctor Albán <victor.af1104@gmail.com>
2021-12-16 21:06:40 +0100

    Añade ejemplos de servicios y observables

--- ej-angular/ejemplos-angular/src/app/app.component.ts
+++ ej-angular/ejemplos-angular/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/cmp07-servicios/services/auth/auth.service';
+import { EventosService } from 'src/app/cmp07-servicios/services/eventos/eventos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ejemplos-angular';
+export class AppComponent implements OnInit {
+  isLoggedIn: boolean = false;
+
+  constructor(private auth: AuthService, private eventos: EventosService) {
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.auth.hasToken();
+
+    this.eventos.authEvent$.subscribe((isLoggedIn: boolean) => {
+      console.log('Event Emitter', isLoggedIn);
+
+      this.isLoggedIn = isLoggedIn;
+    });
+
+    this.eventos.authEvent$.emit(this.auth.hasToken());
+  }
+
+  login(): void {
+    const token = Math.random().toString().slice(2);
+
+    this.auth.setToken(token);
+  }
+
+  logout(): void {
+    this.auth.delToken();
+  }
 }
