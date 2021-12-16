cbc636290c6738e45be8f801de0c50cc643b6885
Víctor Albán <victor.af1104@gmail.com>
2021-12-16 08:31:23 +0100

    Actualiza la importación de componentes y módulos

--- ej-angular/ejemplos-angular/src/app/app.module.ts
+++ ej-angular/ejemplos-angular/src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { Cmp03InputYOutputComponent } from 'src/app/cmp03-input-y-output/cmp03-input-y-output.component';
+import { MarcarDirective } from 'src/app/cmp04-directivas/marcar/marcar.directive';
 import { AppComponent } from './app.component';
 import { Cmp01DataBindingComponent } from './cmp01-data-binding/cmp01-data-binding.component';
 import { Cmp02VariablesDePlantillaComponent } from './cmp02-variables-de-plantilla/cmp02-variables-de-plantilla.component';
 import { ReproductorComponent } from './cmp02-variables-de-plantilla/reproductor/reproductor.component';
-import { Cmp03InputYOutputComponent } from 'src/app/cmp03-imput-y-output/cmp03-input-y-output.component';
-import { SugusComponent } from './cmp03-imput-y-output/sugus/sugus.component';
+import { LeiaComponent } from './cmp03-input-y-output/leia/leia.component';
+import { LukeComponent } from './cmp03-input-y-output/luke/luke.component';
+import { MiBotonComponent } from './cmp03-input-y-output/mi-boton/mi-boton.component';
+import { SugusComponent } from './cmp03-input-y-output/sugus/sugus.component';
+import { Cmp04DirectivasComponent } from './cmp04-directivas/cmp04-directivas.component';
+import { PowerModeDirective } from './cmp04-directivas/power-mode/power-mode.directive';
+import { Cmp05PipesComponent } from './cmp05-pipes/cmp05-pipes.component';
+import { DoblePipe } from './cmp05-pipes/pipes/doble/doble.pipe';
+import { FiltroPipe } from './cmp05-pipes/pipes/filtro/filtro.pipe';
+import { OcultarPalabrasPipe } from './cmp05-pipes/pipes/ocultar-palabras/ocultar-palabras.pipe';
+import { ReversePipe } from './cmp05-pipes/pipes/reverse/reverse.pipe';
+import { Cmp06FormulariosComponent } from './cmp06-formularios/cmp06-formularios.component';
+import { DePlantillaComponent } from './cmp06-formularios/de-plantilla/de-plantilla.component';
+import { ReactivoComponent } from './cmp06-formularios/reactivo/reactivo.component';
+import { ErrorComponent } from './cmp06-formularios/error/error.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +30,28 @@ import { SugusComponent } from './cmp03-imput-y-output/sugus/sugus.component';
     Cmp02VariablesDePlantillaComponent,
     ReproductorComponent,
     Cmp03InputYOutputComponent,
-    SugusComponent
+    SugusComponent,
+    MiBotonComponent,
+    LukeComponent,
+    LeiaComponent,
+    Cmp04DirectivasComponent,
+    MarcarDirective,
+    MarcarDirective,
+    PowerModeDirective,
+    Cmp05PipesComponent,
+    DoblePipe,
+    FiltroPipe,
+    ReversePipe,
+    OcultarPalabrasPipe,
+    Cmp06FormulariosComponent,
+    DePlantillaComponent,
+    ReactivoComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
